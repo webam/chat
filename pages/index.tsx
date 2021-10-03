196d4d084d65d4449d7f9282315de4a0ba7ea49c
root <webam@outlook.com>
2021-10-03 20:27:22 +0000

    first client chat

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,61 @@
 /** @jsx h */
-import { h, IS_BROWSER, PageConfig, useState } from "../deps.ts";
+import { h, IS_BROWSER, PageConfig, useState, useEffect, useCallback } from "../deps.ts";
+
+interface Message {
+  text: string,
+
+}
 
 export default function Home() {
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [text, setText] = useState("");
+
+  const getMessages = useCallback(async() => {
+    const res = await fetch("https://webam-deno-server.deno.dev/messages");
+    const data = await res.json();
+    setMessages(data);
+  },[])
+  useEffect(() => {
+    getMessages() 
+  }, [])
+const onSendMessage = useCallback(async () => {
+  await fetch("https://webam-deno-server.deno.dev/messages", {
+    method: "POST",
+    headers: {
+      "content-type" : "application/json"
+    },
+    body: JSON.stringify({text})
+  });
+  setText("");
+  getMessages();
+},[text])
+
   return (
     <div>
-      <p>
-        Welcome to `fresh`. Try update this message in the ./pages/index.tsx
-        file, and refresh.
-      </p>
-      <Counter />
-      <p>{IS_BROWSER ? "Viewing browser render." : "Viewing JIT render."}</p>
+      <div>
+        <input type="text" value={text} onChange={(evt: Event) => 
+          setText((evt.target as HTMLInputElement).value)} />
+         <button onClick={onSendMessage}>add</button>
+      {JSON.stringify(messages)}
+      </div>
     </div>
   );
 }
 
-function Counter() {
-  const [count, setCount] = useState(0);
-  return (
-    <div>
-      <p>{count}</p>
-      <button onClick={() => setCount(count - 1)} disabled={!IS_BROWSER}>
-        -1
-      </button>
-      <button onClick={() => setCount(count + 1)} disabled={!IS_BROWSER}>
-        +1
-      </button>
-    </div>
-  );
-}
+// function Counter() {
+//   const [count, setCount] = useState(0);
+//   return (
+//     <div>
+//       <p>{count}</p>
+//       <button onClick={() => setCount(count - 1)} disabled={!IS_BROWSER}>
+//         -1
+//       </button>
+//       <button onClick={() => setCount(count + 1)} disabled={!IS_BROWSER}>
+//         +1
+//       </button>
+//     </div>
+//   );
+// }
 
 export const config: PageConfig = { runtimeJS: true };
 
